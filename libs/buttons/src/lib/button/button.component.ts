363d2c0ad8222d73632bd455783c076a6a4b72fd
Ceisi Peik <ceisi.peik@nortal.com>
2022-08-09 17:03:39 +0300

    feat: add button and input components

---
+++ libs/buttons/src/lib/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ria-poc-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- libs/inputs/src/lib/inputs.module.ts
+++ libs/inputs/src/lib/inputs.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [InputComponent],
 })
 export class InputsModule {}
