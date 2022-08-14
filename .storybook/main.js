d7cab368bc858136306a405cdbb0974cebcf34fe
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2022-08-14 19:42:32 +0300

    Added HTML addon to Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@whitespace/storybook-addon-html'
+  ],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
