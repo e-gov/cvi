46dec6e5cc1be43266d353708e573bd103e388ac
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2022-08-14 13:37:41 +0300

    Removed scoped styles from Button and Input components because all styles must be global

--- libs/ui/src/lib/button/button.component.ts
+++ libs/ui/src/lib/button/button.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ria-poc-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
   constructor() {}
--- libs/ui/src/lib/input/input.component.ts
+++ libs/ui/src/lib/input/input.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ria-poc-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements OnInit {
   constructor() {}
