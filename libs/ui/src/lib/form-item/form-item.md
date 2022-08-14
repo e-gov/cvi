71bf2a2cc1d6beafc9382e072257a6c79f81b5ea
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2022-08-15 01:38:40 +0300

    Added FormItem component

---
+++ libs/ui/src/lib/form-item/form-item.md
@@ -0,0 +1,14 @@
+## General description
+
+Feature | Description
+- | -
+Selector | `ria-poc-form-item`
+Component name | `FormItemComponent`
+Design system name | Form elements
+Content | yes
+Permitted parent/ancestors | any
+Permitted children | `ria-poc-input`
+
+## Where to use
+
+Use as a field wrapper for form elements.
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { InputComponent } from './input/input.component';
+import { FormItemComponent } from './form-item/form-item.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonComponent, InputComponent],
-  exports: [ButtonComponent, InputComponent],
+  declarations: [ButtonComponent, InputComponent, FormItemComponent],
+  exports: [ButtonComponent, InputComponent, FormItemComponent],
 })
 export class UiModule {}
