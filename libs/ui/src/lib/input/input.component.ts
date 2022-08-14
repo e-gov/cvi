b7ef738d9ab1acbfa1dd2a5ab9c65b582c634005
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2022-08-15 00:26:58 +0300

    Added disabled and htmlId props to Input component

--- libs/ui/src/lib/input/input.component.ts
+++ libs/ui/src/lib/input/input.component.ts
@@ -1,11 +1,17 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'ria-poc-input',
   templateUrl: './input.component.html',
 })
 export class InputComponent {
+  /** HTML id passed from FormItem component */
+  @Input() htmlId!: string;
+
+  /** Button is disabled */
+  @Input() disabled = false;
+
   @HostBinding('class') get getHostClasses(): string {
-    return `veera-textfield`;
+    return `veera-textfield${this.disabled ? ' veera-textfield--is-disabled' : ''}`;
   }
 }
