7e2be339c802e20674cb8b4601ea3c3ed0130032
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2022-08-14 22:59:53 +0300

    Added compodoc for automatic generation of prop tables and inferring possible values of props

--- libs/storybook/.storybook/preview.js
+++ libs/storybook/.storybook/preview.js
@@ -1,6 +1,11 @@
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from './documentation.json';
+
+setCompodocJson(docJson);
+
 export const parameters = {
   docs: {
-    extractComponentDescription: (component, { notes }) => {
+    extractComponentDescription: (_, { notes }) => {
       if (notes) {
         return typeof notes === 'string' ? notes : notes.markdown || notes.text;
       }
