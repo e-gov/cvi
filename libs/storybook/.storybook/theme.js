b4b4c5b74db8d5e4ec9c60ce4bcf4506f1d50c5e
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2022-08-14 20:59:33 +0300

    Added custom theming for the Storybook

---
+++ libs/storybook/.storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  colorSecondary: '#005aa3',
+  brandTitle: 'Veera',
+  brandUrl: 'https://veera.eesti.ee',
+  brandImage: 'https://svgshare.com/i/k2e.svg',
+  brandTarget: '_self',
+});
