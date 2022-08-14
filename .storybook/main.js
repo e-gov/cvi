ea5a0e9a14d7a47ffbdef6ae9b2c2c0e3607c306
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2022-08-14 19:47:00 +0300

    Added Accessibility addon to Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,8 @@ module.exports = {
   stories: [],
   addons: [
     '@storybook/addon-essentials',
-    '@whitespace/storybook-addon-html'
+    '@whitespace/storybook-addon-html',
+    '@storybook/addon-a11y'
   ],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
