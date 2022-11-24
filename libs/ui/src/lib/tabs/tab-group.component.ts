52991ae1082af093ad70524ce1216d06ab70f7d7
Markus Saarniit <markus.saarniit@nortal.com>
2022-11-24 14:03:28 +0200

    feat: fix tab group
    Merge in SUN/veera-components from feature-PH-115-fix-tab-group to master
    
    Squashed commit of the following:
    
    commit f7548df72e3117ffcedaaf2a3379a92e41e0196b
    Author: Markus Saarniit <markus.saarniit@nortal.com>
    Date:   Wed Nov 23 15:25:04 2022 +0200
    
        feat: linting
    
    commit 58dacb4bc45111c5216d58309411b4a2bd33f5a3
    Author: Markus Saarniit <markus.saarniit@nortal.com>
    Date:   Wed Nov 23 15:24:17 2022 +0200
    
        feat: fix tab group

--- libs/ui/src/lib/tabs/tab-group.component.ts
+++ libs/ui/src/lib/tabs/tab-group.component.ts
@@ -1,20 +1,38 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   ContentChildren,
+  OnDestroy,
   QueryList,
 } from '@angular/core';
 import { TabComponent } from './tab.component';
+import { merge, Subscription } from 'rxjs';
 
 @Component({
   selector: 'veera-ng-tab-group',
   templateUrl: './tab-group.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TabGroupComponent {
+export class TabGroupComponent implements AfterViewInit, OnDestroy {
   @ContentChildren(TabComponent) allTabs!: QueryList<TabComponent>;
 
   activeIndex = 0;
+
+  private tabChangesSubscription = Subscription.EMPTY;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.tabChangesSubscription = merge(
+      ...this.allTabs.map((tab) => tab._stateChanges)
+    ).subscribe(() => this.cd.markForCheck());
+  }
+
+  ngOnDestroy() {
+    this.tabChangesSubscription.unsubscribe();
+  }
 
   makeActive(index: number) {
     this.activeIndex = index;
