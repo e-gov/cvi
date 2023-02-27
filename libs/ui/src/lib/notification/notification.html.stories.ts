f7fc2c9ba242ad246112c971d5d9b9015368b0e7
Aleksandr Beliaev <cioe@posteo.si>
2023-02-27 09:04:08 +0200

    fix(ui): moved icon height value from notification Sass component to iconHeight or height Angular prop or inline style on a svg tag (#20)
    Co-authored-by: Aleksandr Beliaev <aleksandr.beliaev@nortal.com>

--- libs/ui/src/lib/notification/notification.html.stories.ts
+++ libs/ui/src/lib/notification/notification.html.stories.ts
@@ -13,7 +13,7 @@ const Template: Story = (args) => ({
     <div class="cvi-notification cvi-notification--severity-info">
       <button aria-label="Close" type="button" class="cvi-notification__close-button">
         <div class="cvi-notification__close-button-inner">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" class="cvi-notification__close-button-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" class="cvi-notification__close-button-icon" height="16px">
             <path d="M14 1.4 12.6 0 7 5.6 1.4 0 0 1.4 5.6 7 0 12.6 1.4 14 7 8.4l5.6 5.6 1.4-1.4L8.4 7 14 1.4z"></path>
           </svg>
         </div>
@@ -21,7 +21,7 @@ const Template: Story = (args) => ({
       <div class="cvi-notification__inner">
         <div class="cvi-notification__header">
           <div class="cvi-notification__header-icon-wrapper">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39.8 40" class="cvi-notification__header-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39.8 40" class="cvi-notification__header-icon" height="24px">
               <path d="M13.3 1.1C-3.2 7-4.7 29.4 10.8 37.8c9.3 5 21.6 1 26.9-8.8 2-3.8 2.3-5.5 1.9-10.4C38.4 5.2 25.8-3.2 13.3 1.1zm16.2 6.4c8.8 6.7 8.4 19.1-1 26-3.9 2.9-13.3 2.9-17.2 0-5.3-4-7-7.2-7-13.4 0-4.8.5-6.4 2.9-9.5 1.6-2.1 4.3-4.4 6-5.2 4.7-2.3 11.9-1.3 16.3 2.1z"></path>
               <circle cx="19.9" cy="12" r="2"></circle>
               <ellipse cx="19.9" cy="24" rx="2" ry="6"></ellipse>
@@ -38,4 +38,3 @@ const Template: Story = (args) => ({
 });
 
 export const Default = Template.bind({});
-Default.args = {};
