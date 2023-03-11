79f91d6b5001b3afcc56919e35826a46908a086a
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-03-12 00:25:43 +0200

    format(cypress): autoformatting

--- apps/ui-e2e/src/component/generated-table-of-contents.component.cy.ts
+++ apps/ui-e2e/src/component/generated-table-of-contents.component.cy.ts
@@ -4,21 +4,36 @@ describe('GeneratedTableOfContentsComponent', () => {
   });
 
   it('Renders generated table of contents', () => {
-    cy.loadStory('Angular Table of contents Generated table of contents', 'Default')
-      .shouldHaveClasses('cvi-ng-table-of-contents-wrapper', 'cvi-table-of-contents')
+    cy.loadStory(
+      'Angular Table of contents Generated table of contents',
+      'Default'
+    )
+      .shouldHaveClasses(
+        'cvi-ng-table-of-contents-wrapper',
+        'cvi-table-of-contents'
+      )
       .within(() => {
         cy.shouldHaveClasses('div', 'cvi-table-of-contents__aside');
       });
   });
 
   it('Highlights an item in the sidebar on scroll', () => {
-    cy.loadStory('Angular Table of contents Generated table of contents', 'Default');
+    cy.loadStory(
+      'Angular Table of contents Generated table of contents',
+      'Default'
+    );
     cy.get('[data-cy="test-h3"]').scrollIntoView();
-    cy.shouldHaveClasses('cvi-ng-table-of-contents-item:nth-child(2n)', 'is-current');
+    cy.shouldHaveClasses(
+      'cvi-ng-table-of-contents-item:nth-child(2n)',
+      'is-current'
+    );
   });
 
   it('Sidebar stays visible after scroll', () => {
-    cy.loadStory('Angular Table of contents Generated table of contents', 'Default');
+    cy.loadStory(
+      'Angular Table of contents Generated table of contents',
+      'Default'
+    );
     cy.get('[data-cy="test-nested-h2"]').scrollIntoView();
     cy.get('cvi-ng-table-of-contents').should('be.visible');
   });
