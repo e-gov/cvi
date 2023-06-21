3eaf180f85562dbe2dbd33fc91d8d737585ad2e0
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-06-22 00:44:20 +0300

    refactor(storybook): slimmed FormItemComponent (Angular) story down

--- libs/ui/src/lib/form-item/form-item.component.stories.ts
+++ libs/ui/src/lib/form-item/form-item.component.stories.ts
@@ -1,4 +1,4 @@
-import { StoryFn, Meta } from '@storybook/angular';
+import { Meta, componentWrapperDecorator } from '@storybook/angular';
 import notes from './form-item.component.md';
 import { FormItemComponent } from './form-item.component';
 
@@ -11,51 +11,31 @@ export default {
     label: 'Example label',
     placeholder: 'Placeholder text...',
   },
+  decorators: [
+    componentWrapperDecorator(FormItemComponent, ({ args }) => {
+      return args;
+    }),
+  ],
 } as Meta<FormItemComponent>;
 
-const Template: StoryFn<FormItemComponent> = (args: FormItemComponent) => ({
-  props: args,
-  /* template */
-  template: `
-    <cvi-ng-form-item [labelPosition]="labelPosition"
-                      [isLabelHidden]="isLabelHidden"
-                      [required]="required"
-                      [label]="label"
-                      [labelId]="labelId"
-                      [htmlId]="htmlId">
-      <cvi-ng-input [placeholder]="placeholder"
-                    [htmlId]="htmlId">
-      </cvi-ng-input>
-    </cvi-ng-form-item>
-  `,
-});
-
 export const Default = {
-  render: Template,
+  render: (args: FormItemComponent) => ({
+    props: args,
+    /* template */
+    template: `
+      <cvi-ng-input [placeholder]="placeholder" [htmlId]="htmlId"></cvi-ng-input>
+    `,
+  }),
 };
 
-const WithTextareaTemplate: StoryFn<FormItemComponent> = (
-  args: FormItemComponent
-) => ({
-  props: args,
-  /* template */
-  template: `
-    <cvi-ng-form-item [labelPosition]="labelPosition"
-                      [isLabelHidden]="isLabelHidden"
-                      [required]="required"
-                      [label]="label"
-                      [labelId]="labelId"
-                      [htmlId]="htmlId">
-      <cvi-ng-textarea [placeholder]="placeholder"
-                       [htmlId]="htmlId">
-      </cvi-ng-textarea>
-    </cvi-ng-form-item>
-  `,
-});
-
 export const WithTextarea = {
-  render: WithTextareaTemplate,
-
+  render: (args: FormItemComponent) => ({
+    props: args,
+    /* template */
+    template: `
+      <cvi-ng-textarea [placeholder]="placeholder" [htmlId]="htmlId"></cvi-ng-textarea>
+    `,
+  }),
   parameters: {
     chromatic: { disableSnapshot: true },
   },
