b1989771d3ad50d20430d2e481fcca0692b3a9fe
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-06-22 00:31:13 +0300

    refactor(storybook): slimmed FeedbackComponent (Angular) story down

--- libs/ui/src/lib/feedback/feedback/feedback.component.stories.ts
+++ libs/ui/src/lib/feedback/feedback/feedback.component.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from '@storybook/angular';
+import { Meta } from '@storybook/angular';
 import notes from './feedback.component.md';
 import { FeedbackComponent } from './feedback.component';
 
@@ -8,39 +8,15 @@ export default {
   component: FeedbackComponent,
 } as Meta<FeedbackComponent>;
 
-const title = 'Did you find what you were looking for?';
-const options = [
-  { value: 'NO', label: 'No' },
-  { value: 'PARTIALLY', label: 'Partially' },
-  { value: 'YES', label: 'Yes' },
-];
-
-const Template: StoryFn<FeedbackComponent> = (args: FeedbackComponent) => ({
-  props: args,
-});
-
 export const Default = {
-  render: Template,
-
-  args: {
-    title,
-    options,
-    showIcons: false,
-  },
+  render: (args: FeedbackComponent) => ({
+    props: args,
+  }),
 };
 
-const WithIconsTemplate: StoryFn<FeedbackComponent> = (
-  args: FeedbackComponent
-) => ({
-  props: args,
-});
-
 export const WithIcons = {
-  render: WithIconsTemplate,
-
+  ...Default,
   args: {
-    title,
-    options,
     showIcons: true,
   },
 };
