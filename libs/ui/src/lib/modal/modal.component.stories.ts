d75200ea9d85861f854df87433c827e5ab3da223
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-07-28 17:32:26 +0300

    refactor(storybook): slimmed down ModalComponent (Angular) and cvi-modal stories

--- libs/ui/src/lib/modal/modal.component.stories.ts
+++ libs/ui/src/lib/modal/modal.component.stories.ts
@@ -1,4 +1,4 @@
-import { StoryFn, Meta } from '@storybook/angular';
+import { Meta } from '@storybook/angular';
 import { ModalComponent } from './modal.component';
 
 export default {
@@ -6,23 +6,22 @@ export default {
   component: ModalComponent,
 } as Meta<ModalComponent>;
 
-const Template: StoryFn<ModalComponent> = (args: ModalComponent) => ({
-  props: args,
-  template: `
-    <ng-template #modal let-testModalTitle="title">
-      <h1>{{ testModalTitle }}</h1>
-      <p dataAttribute="test-content">Modal content</p>
-    </ng-template>
-    <cvi-ng-button [cviNgModal]="modal" modalTitle="Some title" dataAttribute="test-button">Open modal</cvi-ng-button>
-  `,
-});
-
 export const Default = {
-  render: Template,
+  render: (args: ModalComponent) => ({
+    props: args,
+    /* template */
+    template: `
+      <ng-template #modal let-testModalTitle="title">
+        <h1>{{ testModalTitle }}</h1>
+        <p dataAttribute="test-content">Modal content</p>
+      </ng-template>
+      <cvi-ng-button [cviNgModal]="modal" modalTitle="Some title" dataAttribute="test-button">Open modal</cvi-ng-button>
+    `,
+  }),
 };
 
 export const Mobile = {
-  render: Template,
+  ...Default,
 
   parameters: {
     viewport: {
@@ -31,23 +30,20 @@ export const Mobile = {
   },
 };
 
-const TemplateWithModalInsideModal: StoryFn<ModalComponent> = (
-  args: ModalComponent
-) => ({
-  props: args,
-  template: `
-    <ng-template #modal1>
-      <p>Modal 1 content</p>
-      <ng-template #modal2 let-testModalTitle="title">
-        <h1>{{ testModalTitle }}</h1>
-        <p>Modal 2 content</p>
-      </ng-template>
-      <cvi-ng-button [cviNgModal]="modal2" modalTitle="Some title for the inner modal">Open modal 2 inside modal 1</cvi-ng-button>
-    </ng-template>
-    <cvi-ng-button [cviNgModal]="modal1">Open modal</cvi-ng-button>
-  `,
-});
-
 export const WithModalInsideModal = {
-  render: TemplateWithModalInsideModal,
+  render: (args: ModalComponent) => ({
+    props: args,
+    /* template */
+    template: `
+      <ng-template #modal1>
+        <p>Modal 1 content</p>
+        <ng-template #modal2 let-testModalTitle="title">
+          <h1>{{ testModalTitle }}</h1>
+          <p>Modal 2 content</p>
+        </ng-template>
+        <cvi-ng-button [cviNgModal]="modal2" modalTitle="Some title for the inner modal">Open modal 2 inside modal 1</cvi-ng-button>
+      </ng-template>
+      <cvi-ng-button [cviNgModal]="modal1">Open modal</cvi-ng-button>
+    `,
+  }),
 };
--- libs/ui/src/lib/modal/modal.html.stories.ts
+++ libs/ui/src/lib/modal/modal.html.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from '@storybook/angular';
+import { Meta } from '@storybook/angular';
 import notes from './modal.html.md';
 
 export default {
@@ -9,37 +9,35 @@ export default {
   },
 } as Meta;
 
-const Template: StoryFn = (args) => ({
-  props: args,
-  /* template */
-  template: `
-    <div class="cvi-modal">
-      <div class="cvi-modal__dialog" role="dialog" aria-label="Hi!">
-        <button class="cvi-modal__close" aria-label="Close">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" width="14px">
-            <path d="M14,1.4L12.6,0L7,5.6L1.4,0L0,1.4L5.6,7L0,12.6L1.4,14L7,8.4l5.6,5.6l1.4-1.4L8.4,7L14,1.4z"/>
-          </svg>
-        </button>
-        <h2 class="cvi-modal__title">Hi!</h2>
-        <div class="cvi-modal__content">
-          <p>I'm modal</p>
-        </div>
-        <div class="cvi-modal__footer">
-          <div class="cvi-track cvi-track--direction-vertical-reversed-on-mobile cvi-track--layout-flex" style="--gap:2; --horizontal-alignment: flex-end;">
-            <div class="cvi-button cvi-button--appearance-secondary cvi-button--size-m">
-              <button type="button" class="cvi-button__button">Cancel</button>
-            </div>
-            <div class="cvi-button cvi-button--appearance-primary cvi-button--size-m">
-              <button type="button" class="cvi-button__button">Confirm</button>
+export const Default = {
+  render: (args: unknown) => ({
+    props: args,
+    /* template */
+    template: `
+      <div class="cvi-modal">
+        <div class="cvi-modal__dialog" role="dialog" aria-label="Hi!">
+          <button class="cvi-modal__close" aria-label="Close">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" width="14px">
+              <path d="M14,1.4L12.6,0L7,5.6L1.4,0L0,1.4L5.6,7L0,12.6L1.4,14L7,8.4l5.6,5.6l1.4-1.4L8.4,7L14,1.4z"/>
+            </svg>
+          </button>
+          <h2 class="cvi-modal__title">Hi!</h2>
+          <div class="cvi-modal__content">
+            <p>I'm modal</p>
+          </div>
+          <div class="cvi-modal__footer">
+            <div class="cvi-track cvi-track--direction-vertical-reversed-on-mobile cvi-track--layout-flex" style="--gap:2; --horizontal-alignment: flex-end;">
+              <div class="cvi-button cvi-button--appearance-secondary cvi-button--size-m">
+                <button type="button" class="cvi-button__button">Cancel</button>
+              </div>
+              <div class="cvi-button cvi-button--appearance-primary cvi-button--size-m">
+                <button type="button" class="cvi-button__button">Confirm</button>
+              </div>
             </div>
           </div>
         </div>
+        <div class="cvi-modal__backdrop"></div>
       </div>
-      <div class="cvi-modal__backdrop"></div>
-    </div>
-  `,
-});
-
-export const Default = {
-  render: Template,
+    `,
+  }),
 };
