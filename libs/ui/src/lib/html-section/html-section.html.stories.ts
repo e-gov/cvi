47bc8f127349ef3b4b0cd05c8c6f35d02dd7cd03
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-07-28 17:23:43 +0300

    refactor(storybook): slimmed down cvi-html-section stories

--- libs/ui/src/lib/html-section/html-section.html.stories.ts
+++ libs/ui/src/lib/html-section/html-section.html.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from '@storybook/angular';
+import { Meta } from '@storybook/angular';
 import notes from './html-section.html.md';
 
 export default {
@@ -9,34 +9,32 @@ export default {
   },
 } as Meta;
 
-const Template: StoryFn = (args) => ({
-  props: args,
-  /* template */
-  template: `
-    <div class="cvi-html-section">
-      <h3 class="cvi-html-section__title">Some title</h3>
-      <div class="cvi-html-section__content">
-        <div class="cvi-html-section__content-elements">
-          <p>Võta ühendust perekonnaseisuasutusega, kus soovid abielu sõlmida</p>
-        </div>
-        <div class="cvi-html-section__content-elements">
-          <p> Ordered <b>list</b></p>
-          <ol>
-            <li>Item 1</li>
-            <li>Item 2</li>
-          </ol>
-          <p> Unordered <i>list</i></p>
-          <ul>
-            <li><a href="#" target="_blank" class="external-link">Item 1</a></li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
+export const Default = {
+  render: (args: unknown) => ({
+    props: args,
+    /* template */
+    template: `
+      <div class="cvi-html-section">
+        <h3 class="cvi-html-section__title">Some title</h3>
+        <div class="cvi-html-section__content">
+          <div class="cvi-html-section__content-elements">
+            <p>Võta ühendust perekonnaseisuasutusega, kus soovid abielu sõlmida</p>
+          </div>
+          <div class="cvi-html-section__content-elements">
+            <p> Ordered <b>list</b></p>
+            <ol>
+              <li>Item 1</li>
+              <li>Item 2</li>
+            </ol>
+            <p> Unordered <i>list</i></p>
+            <ul>
+              <li><a href="#" target="_blank" class="external-link">Item 1</a></li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  `,
-});
-
-export const Default = {
-  render: Template,
+    `,
+  }),
 };
