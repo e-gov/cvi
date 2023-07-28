6ecdc05508114413e9f21a0bce9bc04e474e1377
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-07-28 17:01:49 +0300

    refactor(storybook): slimmed down cvi-content-container stories even more

--- libs/ui/src/lib/content-container/content-container.html.stories.ts
+++ libs/ui/src/lib/content-container/content-container.html.stories.ts
@@ -1,4 +1,4 @@
-import { StoryFn, Meta } from '@storybook/angular';
+import { Meta } from '@storybook/angular';
 import notes from './content-container.html.md';
 
 export default {
@@ -22,16 +22,14 @@ export default {
   },
 } as Meta;
 
-const Template: StoryFn = (args) => ({
-  props: args,
-  /* template */
-  template: `
-    <div class="cvi-content-container">
-      {{ content }}
-    </div>
-  `,
-});
-
 export const Default = {
-  render: Template,
+  render: (args: unknown) => ({
+    props: args,
+    /* template */
+    template: `
+      <div class="cvi-content-container">
+        {{ content }}
+      </div>
+    `,
+  }),
 };
