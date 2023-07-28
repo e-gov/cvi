c725c168e4a0c5cfe6a7ba465835c71b21876062
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-07-28 23:17:14 +0300

    refactor(storybook): slimmed down StepComponent (Angular) story

--- libs/ui/src/lib/steps/step/step.component.stories.ts
+++ libs/ui/src/lib/steps/step/step.component.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from '@storybook/angular';
+import { Meta } from '@storybook/angular';
 import notes from './step.component.md';
 import { StepComponent } from './step.component';
 
@@ -11,28 +11,23 @@ export default {
   },
 } as Meta;
 
-const Template: StoryFn<StepComponent> = (args: StepComponent) => ({
-  component: StepComponent,
-  props: {
-    ...args,
-  },
-  /* template */
-  template: `
-    <cvi-ng-steps title="Abiellumine" [currentStepIndex]="0">
-      <cvi-ng-step dataAttribute="step_1" cviNgStorybookCurrentComponent>
-        <cvi-ng-step-panel title="The title">
-          Some content for the first step
-        </cvi-ng-step-panel>
-      </cvi-ng-step>
-      <cvi-ng-step dataAttribute="step_2" cviNgStorybookCurrentComponent>
-        <cvi-ng-step-panel title="Another step">
-          Some more content
-        </cvi-ng-step-panel>
-      </cvi-ng-step>
-    </cvi-ng-steps>
-  `,
-});
-
 export const Default = {
-  render: Template,
+  render: (args: StepComponent) => ({
+    props: args,
+    /* template */
+    template: `
+      <cvi-ng-steps title="Abiellumine" [currentStepIndex]="0">
+        <cvi-ng-step dataAttribute="step_1" cviNgStorybookCurrentComponent>
+          <cvi-ng-step-panel title="The title">
+            Some content for the first step
+          </cvi-ng-step-panel>
+        </cvi-ng-step>
+        <cvi-ng-step dataAttribute="step_2" cviNgStorybookCurrentComponent>
+          <cvi-ng-step-panel title="Another step">
+            Some more content
+          </cvi-ng-step-panel>
+        </cvi-ng-step>
+      </cvi-ng-steps>
+    `,
+  }),
 };
