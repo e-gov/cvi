98988ff43a455ffcbe4f21938bf92a7b93f92908
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-07-29 00:16:25 +0300

    refactor(storybook): slimmed down cvi-tab-group stories

--- libs/ui/src/lib/tabs/tab-group.html.stories.ts
+++ libs/ui/src/lib/tabs/tab-group.html.stories.ts
@@ -1,4 +1,4 @@
-import { StoryFn, Meta } from '@storybook/angular';
+import { Meta } from '@storybook/angular';
 import notes from './tab-group.html.md';
 
 export default {
@@ -9,36 +9,34 @@ export default {
   },
 } as Meta;
 
-const Template: StoryFn = (args) => ({
-  props: args,
-  /* template */
-  template: `
-    <div class="cvi-tab-group">
-      <div role="tablist" class="cvi-tab-group__list">
-        <button type="button"
-                aria-selected="true"
-                role="tab"
-                id="tabgroup-tab-1"
-                class="cvi-tab-group__tab-btn active"
-                aria-controls="tabgroup-tabpanel-1">Tab 1</button>
-        <button type="button"
-                aria-selected="false"
-                role="tab"
-                id="tabgroup-tab-2"
-                class="cvi-tab-group__tab-btn"
-                aria-controls="tabgroup-tabpanel-2">Tab 2</button>
-      </div>
-      <div class="cvi-tab-group__tab-content"
-           role="tabpanel"
-           id="tabgroup-tabpanel-1"
-           aria-labelledby="tabgroup-tab-1"
-           tabindex="0">
-        First tab content
-      </div>
-    </div>
-  `,
-});
-
 export const Default = {
-  render: Template,
+  render: (args: unknown) => ({
+    props: args,
+    /* template */
+    template: `
+      <div class="cvi-tab-group">
+        <div role="tablist" class="cvi-tab-group__list">
+          <button type="button"
+                  aria-selected="true"
+                  role="tab"
+                  id="tabgroup-tab-1"
+                  class="cvi-tab-group__tab-btn active"
+                  aria-controls="tabgroup-tabpanel-1">Tab 1</button>
+          <button type="button"
+                  aria-selected="false"
+                  role="tab"
+                  id="tabgroup-tab-2"
+                  class="cvi-tab-group__tab-btn"
+                  aria-controls="tabgroup-tabpanel-2">Tab 2</button>
+        </div>
+        <div class="cvi-tab-group__tab-content"
+             role="tabpanel"
+             id="tabgroup-tabpanel-1"
+             aria-labelledby="tabgroup-tab-1"
+             tabindex="0">
+          First tab content
+        </div>
+      </div>
+    `,
+  }),
 };
