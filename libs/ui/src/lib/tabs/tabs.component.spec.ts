186a290840e21cefbbb722dde909fb401fd4598c
Heleri Mändla <heleri.mandla@gmail.com>
2023-08-02 10:02:14 +0300

    feat(storybook): fix test

--- libs/ui/src/lib/tabs/tabs.component.spec.ts
+++ libs/ui/src/lib/tabs/tabs.component.spec.ts
@@ -1,21 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { TabsComponent } from './tabs.component';
+import { DebugElement } from '@angular/core';
 
 describe('CviTabComponent', () => {
   let component: TabsComponent;
   let fixture: ComponentFixture<TabsComponent>;
-
-  const debugTest = fixture.debugElement.queryAll(
-    By.css('.cvi-tabs__list-mobile')
-  );
-  const mobileTabsDropdown = debugTest[0].nativeElement;
+  let debugTest: DebugElement[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TabsComponent],
     }).compileComponents();
 
+    debugTest = fixture.debugElement.queryAll(By.css('.cvi-tabs__list-mobile'));
     fixture = TestBed.createComponent(TabsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -28,6 +26,7 @@ describe('CviTabComponent', () => {
   it('should render mobile dropdown menu when screen size is smaller than md breakpoint', () => {
     global.innerWidth = 500;
     global.dispatchEvent(new Event('resize'));
+    const mobileTabsDropdown = debugTest[0].nativeElement;
 
     expect(mobileTabsDropdown).toBeTruthy();
   });
