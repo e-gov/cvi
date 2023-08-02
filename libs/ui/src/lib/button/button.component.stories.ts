441f62027737c85a6d57720942af109e21ec817a
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-08-02 18:09:30 +0300

    docs(storybook): added example stories for ButtonComponent with icon

--- libs/ui/src/lib/button/button.component.stories.ts
+++ libs/ui/src/lib/button/button.component.stories.ts
@@ -59,6 +59,18 @@ Text.args = {
   appearance: 'text',
 };
 
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  iconName: 'add',
+  iconPosition: 'after',
+};
+
+export const WithIconBefore = Template.bind({});
+WithIconBefore.args = {
+  iconName: 'add',
+  iconPosition: 'before',
+};
+
 const TemplateWithCustomColor: Story<ButtonComponent> = (
   args: ButtonComponent
 ) => ({
@@ -71,36 +83,10 @@ const TemplateWithCustomColor: Story<ButtonComponent> = (
   ],
   /* template */
   template: `
-    <cvi-ng-button [disabled]="disabled" [size]="size" [appearance]="appearance">{{ content }}</cvi-ng-button>
+    <cvi-ng-button [disabled]="disabled" [size]="size" [appearance]="appearance" [iconName]="iconName" [iconPosition]="iconPosition" [iconHeight]="iconHeight">
+      {{ content }}
+    </cvi-ng-button>
   `,
 });
 
 export const WithCustomColor = TemplateWithCustomColor.bind({});
-
-const TemplateTextButtonWithIcon: Story<ButtonComponent> = (
-  args: ButtonComponent
-) => ({
-  props: args,
-  /* template */
-  template: `
-  <cvi-ng-button appearance="text">
-    <cvi-ng-labeled-icon name="add" iconPosition="before" verticalAlignment="center" [iconHeight]="16" [gap]="2">
-      {{ content }}
-    </cvi-ng-labeled-icon>
-  </cvi-ng-button>
-  `,
-});
-
-export const TextButtonWithIcon = TemplateTextButtonWithIcon.bind({});
-
-const TemplateSmallButtonWithIcon: Story<ButtonComponent> = (
-  args: ButtonComponent
-) => ({
-  props: args,
-  /* template */
-  template: `
-  <cvi-ng-button [iconPosition]="iconPosition" [iconName]="'exit_to_app'" [disabled]="disabled" [size]="size" [appearance]="appearance" [gap]="gap">{{ content }}</cvi-ng-button>
-  `,
-});
-
-export const smallButtonWithIcon = TemplateSmallButtonWithIcon.bind({});
