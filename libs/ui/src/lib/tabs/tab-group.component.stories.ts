fa81d7682fcc6b5494af41cade3e8dffeeb519b6
keit.jarve <keit.jarve@nortal.com>
2023-08-17 01:07:55 +0300

    feat: remove tab with form from tabs and tab-group stories

--- libs/ui/src/lib/tabs/tab-group.component.stories.ts
+++ libs/ui/src/lib/tabs/tab-group.component.stories.ts
@@ -81,21 +81,7 @@ const TemplateWithIcon: Story<TabComponent> = (args: TabComponent) => ({
     [gap]="gap"
     >{{ content }}</cvi-ng-tab
   >
-  <cvi-ng-tab title="Tab 2, with a form">
-    <div [formGroup]="form">
-      Textarea, input and the character counter directive should work properly
-      inside tabs.
-      <cvi-ng-form-item [label]="formLabel" [htmlId]="formHtmlId">
-        <cvi-ng-textarea
-          formControlName="text"
-          cviNgCharacterCounter
-          [minRows]="5"
-          [htmlId]="formHtmlId"
-        ></cvi-ng-textarea>
-      </cvi-ng-form-item>
-    </div>
-  </cvi-ng-tab>
-  <cvi-ng-tab title="Tab 3 with lots and lots of random text"
+  <cvi-ng-tab title="Tab 2 with lots and lots of random text"
     >Third tab content. File not found. I was having the most wonderful dream.
     Except you were there, and you were there, and you were there! And when we
     woke up, we had these bodies. I could if you hadn't turned on the light and
