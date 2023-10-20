0d41c32d3181bef46cef139496be15174e345999
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-10-21 00:20:18 +0300

    chore(storybook): removed unnecessary args data from TableCardComponent stories

--- libs/ui/src/lib/table-card/table-card.component.stories.ts
+++ libs/ui/src/lib/table-card/table-card.component.stories.ts
@@ -2,50 +2,14 @@ import { Meta, Story } from '@storybook/angular';
 import notes from './table-card.component.md';
 import { TableCardComponent } from './table-card.component';
 
-const statuses = [
-  {
-    severity: 'info',
-    label: 'Lõppenud',
-  },
-  {
-    severity: 'error',
-    label: 'Katkenud',
-  },
-];
-
-const getStatusBadgeLabelBySeverity = (severity: any) =>
-  statuses.find((st: any) => st.severity === severity)?.label;
-
 export default {
   title: 'Angular/Table Card',
   parameters: { notes },
-  args: {
-    headerLabels: ['Lõppkuupäev', 'Sündmus', 'Staatus'],
-    data: [
-      {
-        event: 'Abielu sõlmimine',
-        statusSeverity: 'info',
-        lastChanged: '2021-07-20',
-        tooltip: 'Lõppenud',
-        button: 'Vaata',
-        icon: 'arrow_b_right',
-      },
-      {
-        event: 'Abielu sõlmimine',
-        statusSeverity: 'error',
-        lastChanged: '2018-07-17',
-        tooltip: 'Katkenud',
-        button: 'Vaata',
-        icon: 'arrow_b_right',
-      },
-    ],
-  },
 } as Meta<TableCardComponent>;
 
 const Template: Story<TableCardComponent> = (args: TableCardComponent) => ({
   props: {
     ...args,
-    getStatusBadgeLabelBySeverity,
   },
   /* template */
   template: `
