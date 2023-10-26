d39c32ee64df4b6e8c96cc8d2e15197a3b3b1a8a
keit.jarve <keit.jarve@nortal.com>
2023-10-26 18:01:48 +0300

    fix(ui): mobile breadcrumb for group service EBS-1147 STPA-268

--- libs/ui/src/lib/breadcrumbs/breadcrumbs.component.ts
+++ libs/ui/src/lib/breadcrumbs/breadcrumbs.component.ts
@@ -18,6 +18,8 @@ export class BreadcrumbsComponent {
   /** Name of an icon in front of breadcrumb */
   @Input() iconName: CviIconName | null = null;
 
+  @Input() isGroupService = false;
+
   @Output() breadcrumbChange = new EventEmitter<number>();
 
   mobileBreadcrumbIndex!: number;
@@ -26,7 +28,10 @@ export class BreadcrumbsComponent {
   // Finds the index of last element starting from penultimate element in the list that has a href.
   // If no such elements exist, returns index of last element.
   public getMobileBreadcrumbIndex(): number {
-    const lastElementIndex = this.breadcrumbs.length - 1;
+    let lastElementIndex = this.breadcrumbs.length - 1;
+    if (this.isGroupService) {
+      lastElementIndex -= 1;
+    }
     for (let index = lastElementIndex - 1; index >= 0; index--) {
       if (index !== lastElementIndex && this.breadcrumbs[index].href) {
         return index;
