062ce0597e5231331f48536fc45cdf939db5c757
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2024-04-10 13:09:30 +0300

    refactor(ui): rewritten LabeledItemComponent story to CSF3 format

--- libs/ui/src/lib/labeled-item/labeled-item.component.stories.ts
+++ libs/ui/src/lib/labeled-item/labeled-item.component.stories.ts
@@ -1,6 +1,6 @@
 import notes from './labeled-item.component.md';
 import { LabeledItemComponent } from './labeled-item.component';
-import { Meta, Story } from '@storybook/angular';
+import { Meta, StoryObj } from '@storybook/angular';
 
 export default {
   title: 'Angular/Labeled item',
@@ -16,27 +16,27 @@ export default {
     content: 'Abielu sõlmimine',
   },
 } as Meta<LabeledItemComponent>;
+type Story = StoryObj<LabeledItemComponent>;
 
-const Template: Story<LabeledItemComponent> = (args: LabeledItemComponent) => ({
-  props: {
-    ...args,
-  },
-  /* template */
-  template: `
-    <cvi-ng-labeled-item [label]="label">{{ content }}</cvi-ng-labeled-item>
-  `,
-});
+export const Default: Story = {
+  render: (args: LabeledItemComponent) => ({
+    props: args,
+    template: `<cvi-ng-labeled-item [label]="label">{{ content }}</cvi-ng-labeled-item>`,
+  }),
+};
 
-export const Default = Template.bind({});
-
-export const Mobile = Template.bind({});
-Mobile.parameters = {
-  viewport: {
-    defaultViewport: 'iphone12mini',
+export const Mobile: Story = {
+  ...Default,
+  parameters: {
+    viewport: {
+      defaultViewport: 'iphone12mini',
+    },
   },
 };
 
-export const WithoutLabel = Template.bind({});
-WithoutLabel.args = {
-  label: '',
+export const WithoutLabel: Story = {
+  ...Default,
+  args: {
+    label: '',
+  },
 };
