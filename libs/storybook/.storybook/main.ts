3923f8894f7995d2faba0e5e78aaacb8d8c9088a
Ceisi Peik <ceisi.peik@nortal.com>
2024-07-01 10:36:21 +0300

    fix(storybook): colors stories

--- libs/storybook/.storybook/main.ts
+++ libs/storybook/.storybook/main.ts
@@ -5,10 +5,19 @@ const config: StorybookConfig = {
   ...rootMain,
 
   stories: [
-    ...rootMain.stories,
-    '../../**/src/lib/**/*.stories.ts',
-    '../../**/src/lib/**/*.stories.mdx',
+    // ...rootMain.stories,
+    // '../../**/src/lib/**/*.mdx',
+    // '../../**/src/lib/**/*.stories.ts',
+    '../src/lib/stories/angular.stories.mdx',
+    '../src/lib/stories/ng-diagrams.stories.mdx',
+    '../src/lib/stories/icons/installation.stories.mdx',
+    '../src/lib/stories/icons/contributions.stories.mdx',
+    '../src/lib/stories/styles/installation.stories.mdx',
+    '../src/lib/stories/styles/variables/colors.mdx',
+    '../src/lib/stories/styles/variables/colors.stories.ts',
+
   ],
+  exceptions: ['../src/lib/stories/styles/variables/colors.stories.ts'],
   addons: [...rootMain.addons, '@storybook/addon-essentials'],
   staticDirs: ['../src/assets'],
   webpackFinal: async (config, { configType }) => {
