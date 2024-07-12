bd3a82bc61de9eb2d992bab50c50e7b11d3ce3d2
Ceisi Peik <ceisi.peik@nortal.com>
2024-07-12 13:59:35 +0300

    fix(cypress): table and table-card tests

--- apps/ui-e2e/src/component/table.component.cy.ts
+++ apps/ui-e2e/src/component/table.component.cy.ts
@@ -4,29 +4,31 @@ describe('TableComponent', () => {
   });
 
   it('Renders default table', () => {
-    cy.loadStory('Angular Table', 'Default')
-      .find('.cvi-table__inner')
-      .should('exist')
-      .each(($tableInner, index, $tableInnerArray) => {
-        cy.wrap($tableInner).within(() => {
-          cy.get('.cvi-table__table')
-            .should('exist')
-            .within(() => {
-              cy.get('.cvi-table__header-row')
-                .should('exist')
-                .within(() => {
-                  cy.get('.cvi-table__header-cell').should('exist');
-                });
-              cy.wrap($tableInnerArray[index])
-                .find('.cvi-table__body-row')
-                .should('exist')
-                .each(($bodyRow) => {
-                  cy.wrap($bodyRow).within(() => {
-                    cy.get('.cvi-table__body-cell').should('exist');
+    cy.loadStory('Angular Table', 'Default').then(() => {
+      cy.get('cvi-ng-table').should('exist')
+        .find('.cvi-table__inner')
+        .should('exist')
+        .each(($tableInner, index, $tableInnerArray) => {
+          cy.wrap($tableInner).within(() => {
+            cy.get('.cvi-table__table')
+              .should('exist')
+              .within(() => {
+                cy.get('.cvi-table__header-row')
+                  .should('exist')
+                  .within(() => {
+                    cy.get('.cvi-table__header-cell').should('exist');
                   });
-                });
-            });
+                cy.wrap($tableInnerArray[index])
+                  .find('.cvi-table__body-row')
+                  .should('exist')
+                  .each(($bodyRow) => {
+                    cy.wrap($bodyRow).within(() => {
+                      cy.get('.cvi-table__body-cell').should('exist');
+                    });
+                  });
+              });
+          });
         });
-      });
+    });
   });
 });
